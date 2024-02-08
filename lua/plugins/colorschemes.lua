15ed9845fb4ae2202e4c22b9e809ecfdb4d7781e
Blade Boles <bladerboles@gmail.com>
2024-02-08 15:08:19 -0700

    feat: first commit

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,12 @@
+return {
+  "olimorris/onedarkpro.nvim",
+  "marko-cerovac/material.nvim",
+  "catppuccin/nvim",
+  "rebelot/kanagawa.nvim",
+  "EdenEast/nightfox.nvim",
+  "scottmckendry/cyberdream.nvim",
+  "sainnhe/gruvbox-material",
+  "sainnhe/everforest",
+  "AlexvZyl/nordic.nvim",
+  "Mofiqul/dracula.nvim",
+}
