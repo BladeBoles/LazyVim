e5f33645fa7107d99bd26abe21324e4e819c1be6
Blade Boles <bladerboles@gmail.com>
2024-02-08 20:28:36 -0700

    feat: more colorschemes and add glob option for telescope gitrep

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,5 +1,5 @@
 return {
-  "olimorris/onedarkpro.nvim",
+  -- "olimorris/onedarkpro.nvim",
   "marko-cerovac/material.nvim",
   "catppuccin/nvim",
   "rebelot/kanagawa.nvim",
@@ -9,4 +9,18 @@ return {
   "sainnhe/everforest",
   "AlexvZyl/nordic.nvim",
   "Mofiqul/dracula.nvim",
+  -- {
+  --   "drewtempelmeyer/palenight.vim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("palenight")
+  --   end,
+  -- },
+  {
+    -- Theme inspired by Atom
+    "navarasu/onedark.nvim",
+    config = function()
+      vim.cmd.colorscheme("onedark")
+    end,
+  },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,10 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = { path_display = { "smart" } },
-  },
+  "nvim-telescope/telescope-live-grep-args.nvim",
+  -- This will not install any breaking changes.
+  -- For major updates, this must be adjusted manually.
+  version = "^1.0.0",
+  config = function()
+    require("telescope").load_extension("live_grep_args")
+    vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+  end,
 }
