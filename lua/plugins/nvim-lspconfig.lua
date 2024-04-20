0bb3dbacf7e398f95f3b583b6251cd18177a15c1
Blade Boles <bladerboles@gmail.com>
2024-04-19 18:22:19 -0600

    feat: update

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,40 +1,41 @@
-local util = require("lspconfig/util")
-return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      tsserver = {
-        handlers = {
-          ["textDocument/publishDiagnostics"] = function(err, result, ctx, config)
-            if not result or not result.diagnostics then
-              return
-            end
-
-            -- Filter diagnostics here as before
-            local diagnostics = {}
-            for _, diagnostic in ipairs(result.diagnostics) do
-              if diagnostic.code ~= 80001 then
-                table.insert(diagnostics, diagnostic)
-              end
-            end
-
-            -- Ensure 'ctx.bufnr' is used directly and is not nil
-            if ctx.bufnr then
-              vim.diagnostic.set(ctx.client_id, ctx.bufnr, diagnostics, config)
-            else
-              -- TODO: figure out why this happens and do more than swallow the error
-              -- print("Warning: Buffer number (bufnr) is nil in diagnostic handler.")
-            end
-          end,
-        },
-      },
-      volar = {
-        filetypes = {
-          "typescript",
-          "vue",
-        },
-        root_dir = util.root_pattern("web/src/App.vue"),
-      },
-    },
-  },
-}
+return {}
+-- local util = require("lspconfig/util")
+-- return {
+--   "neovim/nvim-lspconfig",
+--   opts = {
+--     servers = {
+--       tsserver = {
+--         handlers = {
+--           ["textDocument/publishDiagnostics"] = function(err, result, ctx, config)
+--             if not result or not result.diagnostics then
+--               return
+--             end
+--
+--             -- Filter diagnostics here as before
+--             local diagnostics = {}
+--             for _, diagnostic in ipairs(result.diagnostics) do
+--               if diagnostic.code ~= 80001 then
+--                 table.insert(diagnostics, diagnostic)
+--               end
+--             end
+--
+--             -- Ensure 'ctx.bufnr' is used directly and is not nil
+--             if ctx.bufnr then
+--               vim.diagnostic.set(ctx.client_id, ctx.bufnr, diagnostics, config)
+--             else
+--               -- TODO: figure out why this happens and do more than swallow the error
+--               -- print("Warning: Buffer number (bufnr) is nil in diagnostic handler.")
+--             end
+--           end,
+--         },
+--       },
+--       volar = {
+--         filetypes = {
+--           "typescript",
+--           "vue",
+--         },
+--         root_dir = util.root_pattern("web/src/App.vue"),
+--       },
+--     },
+--   },
+-- }
