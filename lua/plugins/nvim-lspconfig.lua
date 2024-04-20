1da67f3576d02af674357c234ff5dec341529ac0
Blade Boles <bladerboles@gmail.com>
2024-04-19 23:58:14 -0600

    fix: stupid bs with volar/ts lsps

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,41 +1,37 @@
-return {}
+local util = require("lspconfig/util")
+
+-- This is some old hacky stuff that had to be done because of a
+-- weird error that I think was fixed in an update.  I'm keeping it
+-- in case that reappears.
+
 -- local util = require("lspconfig/util")
--- return {
---   "neovim/nvim-lspconfig",
---   opts = {
---     servers = {
---       tsserver = {
---         handlers = {
---           ["textDocument/publishDiagnostics"] = function(err, result, ctx, config)
---             if not result or not result.diagnostics then
---               return
---             end
---
---             -- Filter diagnostics here as before
---             local diagnostics = {}
---             for _, diagnostic in ipairs(result.diagnostics) do
---               if diagnostic.code ~= 80001 then
---                 table.insert(diagnostics, diagnostic)
---               end
---             end
---
---             -- Ensure 'ctx.bufnr' is used directly and is not nil
---             if ctx.bufnr then
---               vim.diagnostic.set(ctx.client_id, ctx.bufnr, diagnostics, config)
---             else
---               -- TODO: figure out why this happens and do more than swallow the error
---               -- print("Warning: Buffer number (bufnr) is nil in diagnostic handler.")
---             end
---           end,
---         },
---       },
---       volar = {
---         filetypes = {
---           "typescript",
---           "vue",
---         },
---         root_dir = util.root_pattern("web/src/App.vue"),
---       },
---     },
---   },
--- }
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      tsserver = {
+        init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = "/home/gandalf/.nvm/versions/node/v18.12.1/lib/@vue/typescript-plugin",
+              languages = { "javascript", "typescript", "vue" },
+            },
+          },
+        },
+        filetypes = {
+          "javascript",
+          "typescript",
+          "vue",
+        },
+      },
+      volar = {
+        init_options = {
+          vue = {
+            hybridMode = false,
+          },
+        },
+      },
+    },
+  },
+}
